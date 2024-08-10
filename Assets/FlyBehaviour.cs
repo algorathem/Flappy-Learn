1cdeab9292a810352afcb6e98211ddcd5c8b6e87
kcl <kcleck06@gmail.com>
2024-08-10 15:41:56 +0800

    add game over and replay

--- Assets/FlyBehaviour.cs
+++ Assets/FlyBehaviour.cs
@@ -27,4 +27,9 @@ public class FlyBehaviour : MonoBehaviour
     {
         transform.rotation = Quaternion.Euler(0, 0, _rb.velocity.y * _rotationSpeed);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.instance.GameOver();
+    }
+
 }
